927f8092266aede2758c0f1b51bc8e658e40c27d
nwaykole <80094560+nwaykole@users.noreply.github.com>
2024-11-04 11:42:02 +0000

    Create gcpvm.tf

---
+++ gcpvm.tf
@@ -0,0 +1,15 @@
+resource "google_compute_instance" "vm_instance" {
+    name         = "gcptutorials-vm"
+    machine_type = "f1-micro"
+  
+    boot_disk {
+      initialize_params {
+        image = "debian-cloud/debian-9"
+      }
+    }    
+    network_interface {       
+      network = "default"
+      access_config {
+      }
+    }
+  }
