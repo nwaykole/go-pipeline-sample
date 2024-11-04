a0baa29e4cdd7a0d959a6896fbe468f052773ed3
nwaykole <80094560+nwaykole@users.noreply.github.com>
2024-11-04 11:54:57 +0000

    provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 # Specify the provider (GCP, AWS, Azure)
 provider "google" {
-credentials = "${file("credentials.json")}"
-project = "ac-shared-playground"
+#credentials = "${file("credentials.json")}"
+project = "prj-gft-nnb-01"
 region = "us-central1"
 }
