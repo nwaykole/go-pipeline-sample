3bb96a6ff4461fe9f2e6338eb7a87e424fe7b8bf
nwaykole <80094560+nwaykole@users.noreply.github.com>
2024-11-04 12:19:29 +0000

    provider.tf

--- provider.tf
+++ provider.tf
@@ -3,4 +3,14 @@ provider "google" {
 #credentials = "${file("credentials.json")}"
 project = "prj-gft-nnb-01"
 region = "us-central1"
+zone = "us-central1-f"
+}
+
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "6.8.0"
+    }
+  }
 }
